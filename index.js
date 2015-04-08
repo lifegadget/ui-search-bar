875c9496d0dcb6888a064b1e51c837c58bcb5392
Ken  Snyder <ken@ken.net>
2015-04-08 21:20:26 +0100

    add addon CSS

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 'use strict';
 
 module.exports = {
-  name: 'ui-search-bar'
+  name: 'ui-search-bar',
+	description: 'Search bar and optional filter criteria',
+	included: function(app) {
+		this._super.included(app);
+    // specific to this addon
+    app.import('vendor/ui-search-bar/ui-search-bar.css');
 };
